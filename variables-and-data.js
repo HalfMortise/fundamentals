47bfb4a463604bdb8e753fcbc4c87003f66f1003
Half Mortise <HalfMortise@users.noreply.github.com>
2018-06-26 12:23:38 -0600

    Completed

---
+++ variables-and-data.js
@@ -0,0 +1,24 @@
+/*
+ * Variables
+ */
+let wordData = "Word list";
+
+console.log(wordData);
+
+let subTotal = 100;
+let tax = subTotal * 0.07;
+let totalWithTax = tax + subTotal;
+
+/*
+ * Data Types
+ */
+let obj = {
+	key1: "value",
+	key2: 4,
+	boolean: true,
+	obj: {
+		obj2Key1: "value of inner object",
+	}
+};
+
+console.log(obj.key1);
